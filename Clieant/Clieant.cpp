96ddc54b1cde0c907465001deb83323a04796a1e
rocket24h <phanhuyductai@gmail.com>
2022-07-09 19:14:29 +0700

    edit

--- Clieant/Clieant.cpp
+++ Clieant/Clieant.cpp
@@ -5,9 +5,12 @@
 #include "framework.h"
 #include <afxsock.h>
 #include <iostream>
+#include <string>
+#include "date.h"
 #ifdef _DEBUG
 #define new DEBUG_NEW
 #endif
+#include <list>
 
 // The one and only application object
 
@@ -19,16 +22,58 @@ struct user {
 	char* name = NULL;
 	char* password = NULL;
 	void add(char* src, char*& dst) {
+		if (src == NULL) return;
 		if (dst != NULL) delete[] dst;
 		int size = strlen(src);
 		dst = new char[size + 1];
 		strcpy_s((char*)dst, size + 1, src);
 	}
 	char* STK = NULL;
-	user* next;
+	user* next = NULL;
+};
+struct bookingDates
+{
+	char* booked = NULL;
+	char* arrive = NULL;
+	char* leave = NULL;
+	void add(char* src, char*& dst)
+	{
+		if (dst != NULL) delete[] dst;
+		int size = strlen(src);
+		dst = new char[size + 1];
+		strcpy_s((char*)dst, size + 1, src);
+	}
+
+	bookingDates* next;
+};
+struct room
+{
+	char* name = NULL;
+	char* type = NULL;
+	char* description = NULL;
+	char* price = NULL;
+	void add(char* src, char*& dst)
+	{
+		if (dst != NULL) delete[] dst;
+		int size = strlen(src);
+		dst = new char[size + 1];
+		strcpy_s((char*)dst, size + 1, src);
+	}
+	bookingDates* bookedList = NULL;
 };
 
-
+struct hotel
+{
+	char* name;
+	void add(char* src, char*& dst)
+	{
+		if (dst != NULL) delete[] dst;
+		int size = strlen(src);
+		dst = new char[size + 1];
+		strcpy_s((char*)dst, size + 1, src);
+	}
+	room roomList[20] = {};
+};
 void GotoXY(int x, int y)
 {
 	COORD coord;
@@ -56,7 +101,13 @@ bool checkUserName(char* name) {
 			return false;
 		}
 		else if (name[i] > '9') {
-			if (name[i] < 'a' || name[i] > 'z') {
+			if (name[i] < 'A') {
+				return false;
+			}
+			else if (name[i] > 'Z' && name[i] < 'a') {
+				return false;
+			}
+			else if (name[i] > 'z') {
 				return false;
 			}
 		}
@@ -71,8 +122,7 @@ bool  checkPassword(char* password) {
 	return true;
 }
 
-bool checkBankInfo(char* STK)
-{
+bool checkBankInfo(char* STK) {
 	int size = strlen(STK);
 	if (size != 10) return false;
 	for (int i = 0; i < size; i++)
@@ -107,9 +157,304 @@ void drawMenu()
 	GotoXY(16 + 27 + 25, 25); cout << lowerright;
 }
 
-void dangky() {
-	
+void dangnhap(SOCKET* hconnected, int& menuSwitch) {
+	CSocket client;
+	client.Attach(*hconnected);
+	user user1;
+	system("CLS");
+	int temp = 0;
+	int flag = 1;
+	bool legit = 0;
+	int sendAlarm = 0;
+	int timeTL = 12;
+	while (flag <= 2 || timeTL == 0) {
+		switch (flag) {
+		case 1: GotoXY(25 + 20, 21); cout << "Username : ";
+			break;
+		case 2: GotoXY(25 + 20, 22); cout << "Password : ";
+			break;
+		default:
+			break;
+		}
+		int size = 100;
+		int tempSize = 0;
+		char buffer[100];
+		bool checker = 0;
+		cin.getline(buffer, 100);
+		switch (flag) {
+		case 1:
+			if (checkUserName(buffer) == true) {
+				user1.add(buffer, user1.name);
+				sendAlarm = 1;
+				flag++;
+			}
+			else {
+				cout << "Username khong hop le! \n";
+			}
+			break;
+		case 2:
+			if (strlen(buffer) > 3) {
+				user1.add(buffer, user1.password);
+				sendAlarm = 1;
+				flag++;
+			}
+			else {
+				cout << "Mat khau khong hop le! \n";
+			}
+			break;
+		default:
+			break;
+		}
+		if (sendAlarm == 1) {
+			size = strlen(buffer);
+			client.Send(&size, sizeof(size), 0);
+			client.Send(buffer, size, 0);
+			sendAlarm = 0;
+		}
+		timeTL--;
+	}
+	client.Receive((bool*)&legit, sizeof(bool), 0);
+	if (legit == 0) {
+		GotoXY(25 + 20, 20); cout << "Tai khoan khong ton tai" << endl;
+	}
+	else {
+		menuSwitch = 1;
+		GotoXY(25 + 20, 20); cout << "Dang nhap thanh cong" << endl;
+	}
+	(void)getchar();
+	*hconnected = client.Detach();
 }
+
+
+void dangky(SOCKET* hconnected) {
+	CSocket client;
+	client.Attach(*hconnected);
+	user user1;
+	system("CLS");
+	int flag = 1;
+	int sendAlarm = 0;
+	int timeTL = 12;
+	while (flag <= 3) {
+		switch (flag) {
+		case 1: GotoXY(25 + 20, 21); cout << "Username : ";
+			break;
+		case 2:GotoXY(25 + 20, 22); cout << "Password : ";
+			break;
+		case 3:GotoXY(25 + 20, 23); cout << "So tai khoan : ";
+			break;
+		default:
+			break;
+		}
+		int size = 100;
+		int tempSize = 0;
+		char buffer[100];
+		bool checker = 0;
+		cin.getline(buffer, 100);
+		switch (flag) {
+		case 1:
+			if (checkUserName(buffer) == true) {
+				user1.add(buffer, user1.name);
+				sendAlarm = 1;
+			}
+			else {
+				cout << "Username khong hop le! \n";
+			}
+			break;
+		case 2:
+			if (strlen(buffer) > 3) {
+				user1.add(buffer, user1.password);
+				sendAlarm = 1;
+				flag++;
+			}
+			else {
+				cout << "Mat khau khong hop le! \n";
+			}
+			break;
+		case 3:
+			if (strlen(buffer) == 10) {
+				int i = 0;
+				for (i = 0; i < 10; i++) {
+					if (buffer[i] < '0' || buffer[i] > '9') {
+						break;
+					}
+				}
+				if (i == 10) {
+					user1.add(buffer, user1.STK);
+					sendAlarm = 1;
+					flag++;
+				}
+				else {
+					cout << "So tai khoan khong hop le! \n";
+				}
+			}
+			break;
+		default:
+			break;
+		}
+		if (sendAlarm == 1) {
+			size = strlen(buffer);
+			client.Send(&size, sizeof(size), 0);
+			client.Send(buffer, size, 0);
+			if (flag == 1) {
+				client.Receive((bool*)&checker, sizeof(bool), 0);
+				if (checker == false) {
+					flag++;
+				}
+				else {
+					cout << "Ten tai khoan da ton tai moi nhap lai" << endl;
+					continue;
+				}
+			}
+			sendAlarm = 0;
+		}
+		timeTL--;
+		if (timeTL == 0) {
+			break;
+		}
+	}
+	*hconnected = client.Detach();
+}
+void timkiem(SOCKET* hconnected)
+{
+	CSocket client;
+	client.Attach(*hconnected);
+
+	system("CLS");
+
+	int sendAlarm = 0;
+	int timeTL = 12;
+
+
+	int size = 100;
+	int tempSize = 0;
+	char* buffer = new char[12];
+	char* buffer2 = new char[12];
+	date d;
+	date d2;
+	room* li = new room[3];
+	bookingDates f;
+	cout << "Ten khach san : ";
+	char* ht = new char[100];
+	cin.getline(ht, 100);
+	//cout << ht;
+	while (1)
+	{
+		cout << "Ngay vao o :\n ";
+		std::cout << "ngay : ";
+		cin >> d.day;
+		std::cout << "thang : ";
+		cin >> d.month;
+		std::cout << "nam : ";
+		cin >> d.year;
+		if (checkdate(d))
+		{
+
+			convertdate(d, buffer);
+			f.add(buffer, f.arrive);
+			sendAlarm = 1;
+			break;
+		}
+		else {
+			std::cout << "Ngay nhap khong hop le! \n";
+		}
+	}
+	while (1)
+	{
+		cout << "Ngay roi di :\n ";
+		cout << "ngay : ";
+		cin >> d2.day;
+		cout << "thang : ";
+		cin >> d2.month;
+		cout << "nam : ";
+		cin >> d2.year;
+		cout << countNoOfDays(d, d2);
+		if (checkdate(d2) && countNoOfDays(d, d2) >= 0)
+		{
+
+			convertdate(d2, buffer2);
+			f.add(buffer2, f.leave);
+			sendAlarm = 1;
+			break;
+		}
+		else {
+			if (!checkdate)
+			{
+				cout << "Ngay nhap khong hop le! \n";
+			}
+			if (countNoOfDays(d, d2) < 0)
+			{
+				cout << "Ngay den chua toi ma da toi ngay roi di (T_T)\n";
+			}
+
+		}
+	}
+
+	if (sendAlarm == 1) {
+		size = strlen(buffer);
+		client.Send(&size, sizeof(size), 0);
+		client.Send(buffer, size, 0);
+		size = strlen(buffer2);
+		client.Send(&size, sizeof(size), 0);
+		client.Send(buffer2, size, 0);
+		int k;
+		client.Receive((int*)&k, sizeof(int), 0);
+		/// <summary>
+		/// loi nhan du lieu////////////////////////////
+		/// </summary>
+		/// <param name="hconnected"></param>
+		for (int j = 0; j < k; j++)
+		{
+			std::cout << k;
+			int size = 0;
+			int tempSize = 10;
+			char* type;
+			client.Receive((char*)&size, sizeof(int), 0);
+
+			type = new char[size + 1];
+			for (int i = 0; i < size; i = i + tempSize) {
+				if (i + tempSize >= size) {
+					tempSize = size - i;
+				}
+				client.Receive((char*)&type[i], tempSize, 0);
+				cout << type[i];
+			}
+			type[size] = '\0';
+
+			li[j].type = type;
+			cout << type << endl;
+			client.Receive((char*)&size, sizeof(int), 0);
+			char* des;
+			des = new char[size + 1];
+			for (int i = 0; i < size; i = i + tempSize) {
+				if (i + tempSize >= size) {
+					tempSize = size - i;
+				}
+				client.Receive((char*)&des[i], tempSize, 0);
+			}
+			des[size] = '\0';
+			li[j].description = des;
+			cout << des << endl;
+			client.Receive((char*)&size, sizeof(int), 0);
+			char* price;
+			price = new char[size + 1];
+			for (int i = 0; i < size; i = i + tempSize) {
+				if (i + tempSize >= size) {
+					tempSize = size - i;
+				}
+				client.Receive((char*)&price[i], tempSize, 0);
+			}
+			price[size] = '\0';
+			li[j].price = price;
+			cout << price << endl;
+		}
+		///////////////////////////////////////////////////////////////////////////////////////////////
+		system("pause");
+	}
+	sendAlarm = 0;
+
+
+}
+
 
 
 int _tmain(int argc, TCHAR* argv[], TCHAR* envp[])
@@ -137,156 +482,130 @@ int _tmain(int argc, TCHAR* argv[], TCHAR* envp[])
 		CSocket ClientSocket;
 		ClientSocket.Create();
 		// Ket noi den Server
-		int end = 0;
-		if (ClientSocket.Connect(_T("127.0.0.1 "), 1234) != 0) {
+		int end = 1;
+		if (ClientSocket.Connect(_T("127.0.0.1"), 1234) != 0) {
 			FixConsoleWindow();
 			int menu_item = 0, x = 21;
+			int switchMenu = 0;
 			bool menu = true;
-			int flag = 1;
 			while (menu) {
 				drawMenu();
-				GotoXY(18 + 25, x);
-				cout << "-> ";
-				GotoXY(18 + 20 + 25, x);
-				cout << " <-";
+				GotoXY(18 + 25, x); cout << "-> ";
+				GotoXY(18 + 20 + 25, x); cout << " <-";
 				GotoXY(25 + 25, 21);
-				cout << "Dang ky";
+				if (switchMenu == 0) {
+					cout << "Dang ky";
+				}
+				else {
+					cout << "Tra cuu";
+				}
 				GotoXY(25 + 25, 22);
-				cout << "Dang nhap";
-				GotoXY(25 + 25, 23);
-				cout << "Tra cuu";
+				if (switchMenu == 0) {
+					cout << "Dang nhap";
+				}
+				else {
+					cout << "Dat phong";
+				}
+				if (switchMenu != 0) {
+					GotoXY(25 + 25, 23);
+					cout << "Huy phong" << endl;
+				}
 				GotoXY(25 + 25, 24);
 				cout << "Exit";
 				system("pause>nul"); // the >nul bit causes it the print no message
 				if (GetAsyncKeyState(VK_DOWN) && x != 24) { // down button pressed
-					GotoXY(18 + 25, x);
-					cout << "  ";
-					GotoXY(18 + 20 + 25, x);
-					cout << "  ";
-					GotoXY(18 + 21 + 25, x);
-					cout << "  ";
+					GotoXY(18 + 25, x); cout << "  ";
+					GotoXY(18 + 20 + 25, x); cout << "  ";
+					GotoXY(18 + 21 + 25, x); cout << "  ";
 					x++;
-					GotoXY(18 + 25, x);
-					cout << "-> ";
-					GotoXY(18 + 20 + 25, x);
-					cout << " <-";
+					GotoXY(18 + 25, x); cout << "-> ";
+					GotoXY(18 + 20 + 25, x); cout << " <-";
 					menu_item++;
 					continue;
 				}
 				if (GetAsyncKeyState(VK_UP) && x != 21) { // up button pressed
-					GotoXY(18 + 25, x);
-					cout << "  ";
-					GotoXY(18 + 20 + 25, x);
-					cout << "  ";
-					GotoXY(18 + 21 + 25, x);
-					cout << "  ";
+					GotoXY(18 + 25, x); cout << "  ";
+					GotoXY(18 + 20 + 25, x); std::cout << "  ";
+					GotoXY(18 + 21 + 25, x); cout << "  ";
 					x--;
-					GotoXY(18 + 25, x);
-					cout << "-> ";
-					GotoXY(18 + 20 + 25, x);
-					cout << " <-";
+					GotoXY(18 + 25, x); cout << "-> ";
+					GotoXY(18 + 20 + 25, x); cout << " <-";
 					menu_item--;
 					continue;
 				}
 				if (GetAsyncKeyState(VK_RETURN)) { // Enter key pressed
 					ClientSocket.Send(&menu_item, sizeof(menu_item), 0);
-					user user1;
-					int flag = 1; // Đảm bảo gửi đủ 3 gói tin username, mk, STK
-					int sendAlarm = 0; // Báo hiệu là gói tin đã ổn nên gửi đi
-					int timeTL = 12; // hạn tối đa 12 lần nhập cho client
+					SOCKET* hconnected = NULL;
 					switch (menu_item) {
 					case 0:
-						system("CLS");
-						flag = 1;
-						sendAlarm = 0;
-						timeTL = 12;
-						while (flag <= 3) {
-							switch (flag)
-							{
-							case 1: cout << "Username : ";
-								break;
-							case 2: cout << "Password : ";
-								break;
-							case 3: cout << "So tai khoan : ";
-								break;
-							default:
-								break;
-							}
-							int size = 100;
-							int tempSize = 0;
-							char buffer[100];
-							int checker = 0;
-							cin.getline(buffer, 100);
-							switch (flag) {
-							case 1:
-								if (checkUserName(buffer) == true) {
-									user1.add(buffer, user1.name);
-									sendAlarm = 1;
-									if (sendAlarm == 1) {
-										ClientSocket.Receive(&checker, sizeof(bool), 0);
-										if (checker == false) flag++;
-										
-									}
-								}
-								else {
-									cout << "Username khong hop le! \n";
-								}
-								break;
-							case 2:
-								if (strlen(buffer) > 3) {
-									user1.add(buffer, user1.password);
-									sendAlarm = 1;
-									flag++;
-								}
-								else {
-									cout << "Mat khau khong hop le! \n";
-								}
-								break;
-							case 3:
-								if (strlen(buffer) == 10) {
-									int i = 0;
-									for (i = 0; i < 10; i++) {
-										if (buffer[i] < '0' || buffer[i] > '9') {
-											break;
-										}
-									}
-									if (i == 10) {
-										user1.add(buffer, user1.STK);
-										sendAlarm = 1;
-										flag++;
-									}
-									else {
-										cout << "So tai khoan khong hop le! \n";
-									}
-								}
-							default:
-								break;
-							}
-							if (sendAlarm == 1) {
-								size = strlen(buffer);
-								ClientSocket.Send(&size, sizeof(size), 0);
-								ClientSocket.Send(buffer, size, 0);
-								sendAlarm = 0;
-							}
-							timeTL--;
-							if (timeTL == 0) {
-								break;
-							}
+						if (switchMenu == 0)
+						{
+							hconnected = new SOCKET();
+							*hconnected = ClientSocket.Detach();
+							dangky(hconnected);
+							ClientSocket.Attach(*hconnected);
+							delete hconnected;
+							system("CLS");
+							ClientSocket.Send(&end, sizeof(end), 0);
+							system("CLS");
 						}
-						system("CLS");
-						ClientSocket.Send(&end, sizeof(end), 0);
+						else
+						{
+							hconnected = new SOCKET(); // Bien doi cai socket thanh mot bien co the truyen vao ham
+							*hconnected = ClientSocket.Detach();
+							timkiem(hconnected);
+							ClientSocket.Attach(*hconnected);
+							delete hconnected;
+							system("CLS");
+							ClientSocket.Send(&end, sizeof(end), 0);
+						}
+
 						break;
 					case 1:
+						hconnected = new SOCKET(); // Bien doi cai socket thanh mot bien co the truyen vao ham
+						*hconnected = ClientSocket.Detach();
+						dangnhap(hconnected, switchMenu);
+
+						ClientSocket.Attach(*hconnected);
+						delete hconnected;
+						system("CLS");
 						ClientSocket.Send(&end, sizeof(end), 0);
 						break;
 					case 2:
-						ClientSocket.Send(&end, sizeof(end), 0);
-						break;
+					{
+						if (switchMenu == 0)
+						{
+							hconnected = new SOCKET(); // Bien doi cai socket thanh mot bien co the truyen vao ham
+							*hconnected = ClientSocket.Detach();
+							timkiem(hconnected);
+							ClientSocket.Attach(*hconnected);
+							delete hconnected;
+							system("CLS");
+							ClientSocket.Send(&end, sizeof(end), 0);
+							system("CLS");
+						}
+						else
+						{
+							hconnected = new SOCKET(); // Bien doi cai socket thanh mot bien co the truyen vao ham
+							*hconnected = ClientSocket.Detach();
+							timkiem(hconnected);
+							ClientSocket.Attach(*hconnected);
+							delete hconnected;
+							system("CLS");
+							ClientSocket.Send(&end, sizeof(end), 0);
+						}
+
+
+					}
+					break;
 					case 3:
 						end = 0;
 						ClientSocket.Send(&end, sizeof(end), 0);
 						system("CLS");
 						menu = false;
+						break;
+
+					default:
 						break;
 					}
 				}
@@ -298,6 +617,5 @@ int _tmain(int argc, TCHAR* argv[], TCHAR* envp[])
 		// Dong ket noi
 		ClientSocket.Close();
 	}
-
 	return nRetCode;
 }
---
+++ Clieant/date.h
@@ -0,0 +1,195 @@
+#pragma once
+#include <math.h>
+struct date
+{
+	int day, month, year;
+};
+const int monthDays[12] = { 31, 59, 90, 120, 151, 181, 212, 243,273, 304, 334, 365 };
+bool checkdate(date d);
+void convertchargdatetoint(char* s, date& d);
+int countNoOfDays(date date1, date date2);
+date nextday(date d);
+int countLeapYearDays(date d);
+bool checkleapyear(int year);
+int maxday(date d);
+int lengthint(int n);
+#include <iostream>
+#include "pch.h"
+using namespace std;
+bool checkdate(date d)
+{
+	if (d.day <= 0 || d.month <= 0 || d.year < 0) return false;
+	if (d.month > 12) return false;
+	if (d.day > maxday(d)) return false;
+	return true;
+}
+bool checkleapyear(int year)
+{
+	if ((year % 400) == 0) return true;
+	else
+	{
+		if (year % 4 == 0) return true;
+	}
+	return false;
+}
+int maxday(date d)
+{
+	switch (d.month)
+	{
+	case 1: case 3:case 5:case 7:case 8: case 10: case 12:
+	{
+		return 31;
+	}
+	break;
+	case 4:case 6: case 9: case 11:
+	{
+		return 30;
+	}
+	break;
+	case 2:
+	{
+		if (checkleapyear(d.year))
+		{
+			return 29;
+		}
+		else
+		{
+			return 28;
+		}
+	}
+	break;
+	default:
+
+		break;
+	}
+	return -1;
+}
+date nextday(date d)
+{
+	date da;
+	da.day = d.day + 1;
+	da.month = d.month;
+	da.year = d.year;
+	if (d.month == 12)
+	{
+		if (d.day == maxday(d))
+		{
+			da.day = 1;
+			da.month = 1;
+			da.year = d.year + 1;
+		}
+	}
+	else
+	{
+		if (d.day == maxday(d))
+		{
+			da.day = 1;
+			da.month = d.month + 1;
+		}
+	}
+	return da;
+}
+void convertchargdatetoint(char* s, date& d)
+{
+	d.day = (s[0] - '0') * 10 + (s[1] - '0');
+	d.month = (s[3] - '0') * 10 + (s[4] - '0');
+	if (s[9] != '\0')
+	{
+		d.year = (s[6] - '0') * 10 * 10 * 10 + (s[7] - '0') * 10 * 10 + (s[8] - '0') * 10 + (s[9] - '0');
+		return;
+	}
+	if (s[8] != '\0')
+	{
+		d.year = (s[6] - '0') * 10 * 10 + (s[7] - '0') * 10 + (s[8] - '0');
+		return;
+	}
+	if (s[7] != '\0')
+	{
+		d.year = (s[6] - '0') * 10 + (s[7] - '0');
+		return;
+	}
+	if (s[6] != '\0')
+	{
+		d.year = (s[6] - '0');
+		return;
+	}
+}
+
+void convertdate(date d, char*& s)
+{
+	s = new char[11];
+	if (d.day < 10)
+	{
+		s[0] = '0';
+		s[1] = d.day + '0';
+	}
+	else
+	{
+
+		s[0] = d.day / 10 + '0';
+		s[1] = d.day % 10 + '0';
+	}
+	s[2] = '/';
+	if (d.month < 10)
+	{
+		s[3] = '0';
+		s[4] = d.month + '0';
+	}
+	else
+	{
+		s[3] = '1';
+		s[4] = d.month % 10 + '0';
+	}
+	s[5] = '/';
+
+	int i = 5;
+	int y = d.year;
+	while (i < 10)
+	{
+		if ((i + lengthint(d.year)) != 9)
+		{
+			s[++i] = '0';
+		}
+		else
+		{
+
+			for (int j = lengthint(d.year); j > 0; j--)
+			{
+				s[i + j] = y % 10 + '0';
+				y = y / 10;
+			}
+			i = 10;
+		}
+	}
+	s[10] = '\0';
+
+}
+int lengthint(int n)
+{
+	int d = 0;
+	if (n < 10) return 1;
+	while (n > 0)
+	{
+		d++;
+		n = n / 10;
+	}
+	return d;
+}
+
+int countLeapYearDays(date d) {
+	int years = d.year;
+	if (d.month <= 2)
+		years--;
+	return ((years / 4) - (years / 100) + (years / 400));
+}
+int countNoOfDays(date date1, date date2) {
+	long int dayCount1 = (date1.year * 365);
+	dayCount1 += monthDays[date1.month];
+	dayCount1 += date1.day;
+	dayCount1 += countLeapYearDays(date1);
+	long int dayCount2 = (date2.year * 365);
+	dayCount2 += monthDays[date2.month];
+	dayCount2 += date2.day;
+	dayCount2 += countLeapYearDays(date2);
+	return ((dayCount2 - dayCount1));
+}
